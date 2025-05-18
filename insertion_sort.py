5176a6591a3ff08777ceb0dc29e2eb31f39a62d9
Suresh Ghimire <sghimire38288@ucumberlands.edu>
2025-05-17 17:56:32 -0700

    added insertion sort

---
+++ insertion_sort.py
@@ -0,0 +1,14 @@
+def insertion_sort_monotonically(numbers_array):
+    for index in range(1, len(numbers_array)):
+        key = numbers_array[index]
+        left_index = index - 1
+
+        while left_index >= 0 and numbers_array[left_index] < key:
+            numbers_array[left_index + 1] = numbers_array[left_index]
+            left_index -= 1
+        numbers_array[left_index + 1] = key
+
+if __name__ == "__main__":
+    test_data = [5, 2, 9, 1, 5, 6]
+    insertion_sort_monotonically(test_data)
+    print("Sorted in decreasing order:", test_data)
