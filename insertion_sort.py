6c41139c7d050a1ee940c6aea5957cda6c6c2509
Suresh Ghimire <sghimire38288@ucumberlands.edu>
2025-05-17 17:58:27 -0700

    added iteration comment

--- insertion_sort.py
+++ insertion_sort.py
@@ -3,6 +3,7 @@ def insertion_sort_monotonically(numbers_array):
         key = numbers_array[index]
         left_index = index - 1
 
+        # iterate using while loop, moving elements smaller than key to the right
         while left_index >= 0 and numbers_array[left_index] < key:
             numbers_array[left_index + 1] = numbers_array[left_index]
             left_index -= 1
